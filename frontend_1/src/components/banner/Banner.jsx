95adaa8e614f51004d1397b37990b9415e9385b7
Manassaharoy <triptoroy5@gmail.com>
2022-11-17 18:39:25 +0600

    updated links and design

---
+++ frontend_1/src/components/banner/Banner.jsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+// import { styled } from '@mui/material/styles';
+import Box from "@mui/material/Box";
+// import Paper from '@mui/material/Paper';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+// import Stack from '@mui/material/Stack';
+import { styled } from "@mui/material/styles";
+// import './Banner.css';
+import { Container, Paper, Stack } from "@mui/material";
+import bannerimage from "./bannersample.gif";
+import { multiForgotContext } from "../../pages/ForgotContext";
+import { globalContext } from "../../pages/GlobalContext";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const Banner = () => {
+  const videoRef = React.useRef(null);
+  AOS.init({duration:2000});
+   const {t}= React.useContext(globalContext)
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Grid container spacing={2} sx={{
+                
+              
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center",
+              
+              }}>
+          {/* <Grid xs={4}> */}
+            <Grid
+              xs={12}
+              md={10}
+              lg={7}
+              xl={5}
+              sx={{
+                // height: "30vh",
+                mt:"2rem",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+              // data-aos="fade-right"
+            >
+              <Grid >
+                <Box>
+                  <img width={400} src="https://storage.googleapis.com/artifacts.xenon-sentry-364311.appspot.com/assets/homepageContent/mindSchoolHeroSection.webp" alt="" />
+                </Box>
+              </Grid> 
+              <Grid sx={{
+                mt:0
+              }}>
+                <Button href="/registration" size="large" variant="contained">
+                  {t("sign_up_now")}
+                </Button>
+              </Grid>
+            </Grid>
+          {/* </Grid> */}
+          <Grid
+          
+            xs={12}
+            md={10}
+            lg={4}
+            xl={6}
+            sx={{
+              // backgroundImage: `url(${bannerimage})`,
+              // height: "30vh",
+              mt:"2rem",paddingLeft:"1rem",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Box sx={{backgroundColor:"primary.main", borderRadius:"10px",width:"100%",height:"80%",  paddingTop:".8rem", paddingBottom:".5rem", overflow:"hidden"}}>
+            <iframe ref={videoRef} width="100%" height="315" src="https://www.youtube.com/embed/XP6BvzptxR8?autoplay=0&mute=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default Banner;
---
+++ frontend_1/src/pages/AboutUs.js
@@ -0,0 +1,127 @@
+import { Grid, Typography } from '@mui/material';
+import { Box, Container } from '@mui/system';
+import React, { useEffect } from 'react';
+import vdoimage from '../components/downloadApp/Y tr.jpg';
+import instructorData from '../data/instructorData';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+//   width: "50%",
+  width:"50%", height:"315",
+  bgcolor: 'background.paper',
+  border: '1px solid white',
+  borderRadius:"10px",
+  boxShadow: 24,
+  p: 4,
+};
+
+const AboutUs = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [teamMembers, setTeamMembers]=React.useState([
+  {name:"Alim Al Razi", position:"Managing Director",image:"https://storage.googleapis.com/artifacts.xenon-sentry-364311.appspot.com/assets/managementTeam/alim.png"},
+  {name:"Mostafizur Rahman", position:"Chairman",image:"https://storage.googleapis.com/artifacts.xenon-sentry-364311.appspot.com/assets/managementTeam/mostafizurRahman.png"},
+  {name:"Shafayet Jefreey Al Mehdi", position:"CEO and Director",image:"https://storage.googleapis.com/artifacts.xenon-sentry-364311.appspot.com/assets/managementTeam/mehdi.png"},
+  {name:"Moin Mostakim", position:"CTO and Director",image:"https://storage.googleapis.com/artifacts.xenon-sentry-364311.appspot.com/assets/managementTeam/moin.png"}])
+  
+
+    return (
+       <Box>
+        {/* <Container> */}
+          <Box >
+            <Container sx={{display:"flex",justifyContent:"flex-start",flexDirection:{xs:"column", sm:"column",md:"row", lg:"row", xl:"row"},align:"center", p:"2rem", alignItems:"center"}}>
+            {/* <Grid> */}
+            <Box sx={{display:"flex",justifyContent:"flex-start",width:{xs:"100%", sm:"100%", md:"100%", lg:"60%", xl:"60%"}, flexDirection:"column", alignItems:"center"}}>
+                <Typography sx={{fontSize:"2rem", fontWeight:"800", color:"primary.main"}}>
+                Mind <span style={{color:"#f8b100"}}>School</span>
+                </Typography>
+                <Box sx={{width:"70%"}}>
+                <Typography sx={{fontSize:"1.3rem", fontWeight:"500", color:"primary.main"}}>
+                A <span style={{color:"#f8b100", fontWeight:"800"}}>powerful </span><span style={{ fontWeight:"800"}}>platform</span> where you learn how to live a satisfying, miraculous and blissful life
+                </Typography>              
+            </Box>               
+            </Box>
+            <Box>        
+            <img onClick={handleOpen} overflow="hidden" width="100%" height="200" src={vdoimage} alt='' />
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+          
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/XP6BvzptxR8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Box>
+            </Modal>
+            </Box>
+            {/* </Grid> */}
+            </Container>
+          </Box>
+
+          <Box>
+          <Container>
+            <Box sx={{ m: "3rem", textAlign:"justify" }}>
+            <Typography sx={{fontSize:"2rem", fontWeight:"800",textAlign:"center"}}>
+            About Us
+            </Typography>
+            <Typography>
+            A healthy mind is the mantra of a healthy life, healthy physic, and healthy relationship. Mental Health is at top concerns in present era and improving the quality of our life experience is pre-eminent. Mind School is the platform where we aim to teach and train people how to develop your relationship, health, career, mindset and mental wellbeing by illuminating on your own inner intellectual strength and capacity. Here you can learn the prime lessons of your life that matters the most.
+            </Typography>
+            <Typography sx={{mt:"1rem"}}>
+            We desire to bring joy, love and fulfilment in people’s life by sharing this unique gift brought forward by our incredibly talented mind trainers. With the ease of access to internet and technological enhancement, backed by cutting edge IT solutions, we present contents and courses through this powerful platform that can transform you radically. We welcome you to grab this life changing opportunity and surround yourself with positive affirmation.
+            </Typography>
+            </Box>
+            
+              {/* <Box  sx={{display:"flex", flexDirection:"row",
+          }}> */}
+          <Grid sx={{
+            display: "flex",
+            flexDirection: { sm: "column-reverse", lg: "row",xl:"row",md:"row", xs: "column-reverse" }
+          }}>
+            <Grid
+              container
+              // columns={{ xs: 10, sm: 10, md: 10, lg: 10 }}
+              xs={12}
+              // lg={8}
+              justifyContent="center"
+            >
+
+            
+            {
+                    teamMembers.map(member=>
+                    <Box key={member.name} sx={{width: {xs:"70%", sm:"30%", md:"20%", lg:"20%", xl:"20%"}, boxShadow:"5",borderRadius:"10px",display:"flex",
+                    flexDirection:"column", justifyContent:"center", alignItems:"center", margin:"1rem", py:"1rem"}}>  
+                        <Box>  
+                        <img width={100} src={member.image} alt={member.name} />
+                        </Box>                
+                    <Typography>
+                       {member.name}                      
+                    </Typography>
+                    <Typography>
+                       {member.position}                      
+                    </Typography>
+                    <Typography>
+                       Tech Analytica Limited                      
+                    </Typography>
+                    </Box>
+                    )
+                }
+                </Grid>
+            </Grid> 
+            {/* </Box> */}
+             
+            
+        </Container>
+          </Box>
+        {/* </Container> */}
+       </Box>
+    );
+};
+
+export default AboutUs;
