9a89748da60b495fa636eeed4d410ff4df359bdb
Manassaharoy <triptoroy5@gmail.com>
2022-11-17 18:35:33 +0600

    updated links and design

--- backend/Details/responseDetails.js
+++
@@ -1,9 +0,0 @@
-
-export default class ResponseDetails {
-    constructor(data, result) {
-        this.data = data;
-        this.result = result;
-      }
-}
-
-// export default {ResponseDetails};
