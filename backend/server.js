4323e07a181a1bf1524776b8e9fe6db6cb3c59b7
Manassaharoy <triptoroy5@gmail.com>
2022-12-24 16:05:25 +0600

    up

--- backend/server.js
+++ backend/server.js
@@ -136,12 +136,9 @@ app.use(express.json());
 //     credentials: true,
 //   })
 // );
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.mindschoolbd.com/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(
+  cors()
+);
 app.use(
   express.urlencoded({
     extended: true,
