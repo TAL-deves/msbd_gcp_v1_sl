cd4cfbf06819b940c2521c35b1f5998f408dccfe
Manassaharoy <triptoroy5@gmail.com>
2022-12-24 15:58:38 +0600

    up

--- backend/server.js
+++ backend/server.js
@@ -138,7 +138,7 @@ app.use(express.json());
 // );
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "mindschoolbd.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", ["https://mindschoolbd.com/","https://www.mindschoolbd.com/","mindschoolbd.com"]); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
