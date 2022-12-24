556c81969e18e9c2d557607b15bed3ffc9fd1ed5
Manassaharoy <triptoroy5@gmail.com>
2022-12-24 16:41:21 +0600

    up

--- frontend/src/components/login/LoginForm.jsx
+++ frontend/src/components/login/LoginForm.jsx
@@ -142,9 +142,10 @@ const LoginForm = (props) => {
         setCurrentuser(gprofilename);
         setGname(setGname);
         localStorageService.setToken(googleData.data.data);
+        window.localStorage.setItem("access_token", googleData.data.data.access_token)
         // //// console.log(googleData.reslut);
-        // window.opener.location.reload();
-        // window.close();
+        window.opener.location.reload();
+        window.close();
         window.opener.document.location.href = "/courses"
 
         if(googleData.data.data.access_token){
